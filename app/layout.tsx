5de3fd6af4079ae3d7010aaa44d106853ba71dfd
farbod <farbudskate@gmail.com>
2026-08-17 14:12:28 +0430

    shadcn i

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,10 @@
 import type { Metadata } from "next";
 import "./globals.css";
-import { Vazirmatn } from "next/font/google";
+import { Vazirmatn, Geist } from "next/font/google";
 import Header from "@/app/components/layout/Header";
+import { cn } from "@/lib/utils";
+
+const geist = Geist({subsets:['latin'],variable:'--font-sans'});
 
 export const metadata: Metadata = {
   title: "آریا نقش",
@@ -19,7 +22,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="fa" dir="rtl">
+    <html lang="fa" dir="rtl" className={cn("font-sans", geist.variable)}>
       <body
         className={`antialiased bg-[#e0e3ee] text-black vazir.variable font-fa`}
       >
